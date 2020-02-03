27940b4c2333a5c4161a9053f227af657b5338eb
Abbe Keultjes <flauwekeul@gmail.com>
2020-02-03 22:40:28 +0100

    add redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
-import { createStore } from 'redux'
+import { getFirebase } from 'react-redux-firebase'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducer'
 
 const initialState = {}
-export const store = createStore(rootReducer, initialState)
+export const store = createStore(rootReducer, initialState, applyMiddleware(thunk.withExtraArgument(getFirebase)))
